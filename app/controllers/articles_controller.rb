d1d29750f5b724806d020927efec43e0756ddc73
Anirudh Das <dasanirudh5@gmail.com>
2024-07-31 15:31:22 +0530

    added form inputs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,21 @@
 class ArticlesController < ApplicationController
 def show
-
   @article = Article.find(params[:id])
-
 end
  def index
-@articles = Article.all
+  @articles = Article.all
  end
+
+ def new
+  @article = Article.new
+ end
+
+  def create
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get "about", to:'about#index', as: :about
-  resources :articles, only: [:show , :index]
+  resources :articles, only: [:show , :index, :new, :create]
 end
