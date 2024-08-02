7c1c2628509037ae901fb33e4c1639656e01e622
Anirudh Das <dasanirudh5@gmail.com>
2024-08-02 18:10:36 +0530

    starting from grounds up

--- db/migrate/20240802114549_create_users.rb
+++ db/migrate/20240802114549_create_users.rb
@@ -1,9 +1,11 @@
-class CreateUsers < ActiveRecord::Migration[7.1]
+class CreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
-      t.string :email
-      t.string :username
-      t.timestamps
+    unless table_exists?(:users)
+      create_table :users do |t|
+        t.string :name
+        t.string :email
+        t.timestamps
+      end
     end
   end
 end
