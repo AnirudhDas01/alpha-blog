9976fbf51bb2af83b9b7f22b42a05e7074206c9a
Anirudh Das <dasanirudh5@gmail.com>
2024-08-03 11:11:02 +0530

    added users model, and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get "about", to:'about#index', as: :about
   resources :articles
+  get 'signup',to: 'users#new', as: :signup
+  resources :users, expect: [:new]
 end
