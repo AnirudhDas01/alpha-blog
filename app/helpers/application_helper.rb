b61e550cbd2717fcba7e79379680a2a4de1b85cb
Anirudh Das <dasanirudh5@gmail.com>
2024-08-03 14:11:56 +0530

    added user page and listed user 's made articles in personal page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def gravatar_for user
+        email_address = user.email.downcase
+        hash = Digest::SHA256.hexdigest(email_address)
+        gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=200"
+        return image_tag(gravatar_url, alt: user.username , class:"rounded shadow mx-auto d-block mt-4")
+      end
 end
