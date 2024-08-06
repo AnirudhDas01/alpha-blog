1f50be192811ef140f058d419a7d9513c806d0ef
Anirudh Das <dasanirudh5@gmail.com>
2024-08-06 15:30:54 +0530

    added categories controller and CUD function to admin and R function to non admin , and created many-to-many assossiation with article and categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController  < ApplicationController
+   before_action :require_admin, only: [:new, :create, :update, :edit]
 
   def new
     @category = Category.new
@@ -9,18 +10,24 @@ class CategoriesController  < ApplicationController
 
     if  @category.save
       flash[:notice] = "Category Created."
-      redirect_to @category
+      redirect_to categories_path
 
     else
         render :new , status: :unprocessable_entity
 
     end
+  end
+  
+  def update
 
+  end
+
+  def edit
 
   end
 
   def index
-
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
@@ -32,6 +39,12 @@ class CategoriesController  < ApplicationController
     params.require(:category).permit(:name)
   end
 
+  def require_admin
+    if !(logged_in? && current_user.admin? )
+      flash[:alert] = "You are not admin to perform the task"
+      redirect_to categories_path
+    end
+  end
 
 
 end
